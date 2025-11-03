248a095c108bf42749a658663e65f4efa8c569fa
anthonypelaz <antho230104@gmail.com>
2025-11-03 15:24:02 +0100

    remve file that is not use

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Hero from "@/components/hero"
 import Resume from "@/components/resume"
 import Skills from "@/components/skills"
 import Projects from "@/components/projects"
-import Contact from "@/components/contact"
 import Footer from "@/components/footer"
 
 export default function Home() {
